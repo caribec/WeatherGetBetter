358e0da0a0eaf86802963521d0e5db067209d585
caribec <carissabecerra01@yahoo.com>
2025-10-27 21:34:00 -0500

    Initial Commit

---
+++ WeatherGetBetter/WeatherGetBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherGetBetterApp.swift
+//  WeatherGetBetter
+//
+//  Created by Carissa Becerra on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherGetBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
