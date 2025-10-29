d483cac806c45a77627e5aa24c366ab6d20bf502
Alejandro Galvez <159497405+alejalvez@users.noreply.github.com>
2025-10-28 23:05:56 -0500

    Started on hourly details screen

--- WeatherGetBetter/Views/HourlyDetailView.swift
+++ WeatherGetBetter/Views/HourlyDetailView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct HourlyDetailView: View {
+    
+    @State private var selection = 1
+    @State private var searchString: String = ""
+    
     var body: some View {
-        Text("Hourly Detail")
+        VStack() {
+            HStack() {
+                Image(systemName: "clock")
+                Text("Hourly Forecast")
+                    .bold()
+            }
+            HStack() {
+                
+                TextField("", text: $searchString)
+                    .textFieldStyle(.roundedBorder)
+                    .clipShape(.capsule)
+                    .shadow(radius: 4)
+                    .padding()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "gearshape")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.black)
+                        .padding(8)
+                        .background() {
+                            Circle()
+                                .fill(Color.white)
+                                .shadow(radius: 4)
+                        }
+                }
+                .padding()
+            }
+            
+            Picker("", selection: $selection) {
+                Text("Temp").tag(1)
+                Text("Precipitation").tag(2)
+                Text("Humidity").tag(3)
+                Text("Wind").tag(4)
+            }
+            .pickerStyle(.segmented)
+        }
     }
 }
 
