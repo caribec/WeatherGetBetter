5ca8cbea12025df6cd82fcecde7f35017d8fa278
Alejandro Galvez <159497405+alejalvez@users.noreply.github.com>
2025-10-28 20:52:28 -0500

    Started on HomeView

--- WeatherGetBetter/Views/HomeView.swift
+++ WeatherGetBetter/Views/HomeView.swift
@@ -7,13 +7,49 @@
 
 import SwiftUI
 
+
+
 struct HomeView: View {
     @State private var searchString: String = ""
     
     var body: some View {
         VStack() {
             Text("WeatherGetBetter")
-            //TextField($searchString)
+            HStack() {
+                TextField("", text: $searchString)
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "gearshape")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .foregroundColor(.black)
+                }
+                .padding()
+            }
+            
+            GlancePanelView()
+                .padding()
+            
+            Text("Today is a great day to be productive or relax! Be sure to wear sunscreen and drink water if you plan to be out for a while.")
+                .italic()
+                .frame(width: 275)
+                .padding()
+                .background() {
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(Color.black, lineWidth: 2)
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: [Color.gray.opacity(0.1), Color.gray.opacity(0.4)]),
+                                startPoint: .top,
+                                endPoint: .bottom
+                            )
+                        )
+                        .shadow(radius: 10)
+                        .frame(width: 300, height: 100)
+                }
         }
     }
 }
